9e917c0b65e5fb6efe7b90c5b4521ec81f99f5c0
Prit Patel <pritpatel7687@gmail.com>
2024-07-10 15:49:08 +0530

    change 1

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -1,13 +1,19 @@
-import { Link } from "react-router-dom"
-import './Footer.css'
+import { Link } from "react-router-dom";
+import './Footer.css';
+
 const Footer = () => {
     return (
         <>
-
             <footer>
                 <p className="FooterLink">&copy; Prit Patel</p>
+                <div className="footerDetails">
+                    <p>3rd Floor, MY FITNESS ZONE GYM, DR WORLD MALL, Aai Mata Rd, opp. SMC Community Hall, Parvat Patiya, Surat, Gujarat 395010</p>
+                    <p>Mobile: <a href="tel:+918780370532">+91 8780370532</a></p>
+                    <p>Timing: 5:30 am to 10 pm</p>
+                </div>
             </footer>
         </>
-    )
+    );
 }
-export default Footer
+
+export default Footer;
